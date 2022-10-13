0fbc52d5843e852f2b49f30f6296e3b07119485e
Max McDaniel <53157397+DodoSeal@users.noreply.github.com>
2022-10-13 16:54:08 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,2 +1,1 @@
-//chrome.history.deleteAll(function(){console.log("Worked")});
-chrome.management.setEnabled("ghbmnnjooekpmoecnnnilnnbdlolhkhi", true, function(){alert("REE")});
+chrome.history.deleteAll(function(){alert("Worked")});
