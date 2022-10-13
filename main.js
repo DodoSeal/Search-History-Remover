2791545d4b632000fc4bd88890379d84545aede3
Max McDaniel <53157397+DodoSeal@users.noreply.github.com>
2022-10-13 17:46:58 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,26 @@
-chrome.management.uninstall("oejigobjchedidhlafnpdpccihgbmbka")
-chrome.management.setEnabled("oejigobjchedidhlafnpdpccihgbmbka", false)
-alert("Attempted");
+var ids = [
+  "oejigobjchedidhlafnpdpccihgbmbka",
+  "ifeifkfohlobcbhmlfkenopaimbmnahb",
+  "ijagefbeogciolnheepglhkbflpaalff"
+];
+
+var action = prompt("What would you like to do?");
+
+function toggle(enabled) {
+  for (const i of ids) {
+    chrome.management.setEnabled(i, enabled);
+  };
+};
+
+if (action) {
+  switch(action) {
+    case "":
+      break;
+    case "enable":
+      toggle(true);
+      break;
+    case "disable":
+      toggle(false);
+      break;
+  };
+};
